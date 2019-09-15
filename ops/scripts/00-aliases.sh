521bdf29959ab0c454890514fd1bfecb12be2a34
David Vittor <david.vittor@cadreon.com>
2019-09-15 22:50:31 +1000

    Updated scripts

--- ops/scripts/00-aliases.sh
+++ ops/scripts/00-aliases.sh
@@ -3,15 +3,15 @@
 # UTILS
 alias upgrade='sudo apt update && sudo apt -y upgrade'
 
-# AGT Docker commands
-alias mathgame='docker exec -it mathgame /bin/sh -l'
+# MATHGAME Docker commands
+alias math='docker exec -it mathgame /bin/sh -l'
 alias mathstatus='docker ps --filter "name=mathgame"'
 alias mathstart='docker start mathgame'
 alias mathstop='docker stop mathgame'
 alias mathrestart='docker restart mathgame'
 alias mathlog='docker logs -t -f mathgame'
 
-# AGT AWS commands
+# MATHGAME AWS commands
 alias ecrlogin='$(aws --profile mathgame ecr get-login --no-include-email --region ap-southeast-2)'
 alias mathclean='docker rm -f mathgame && docker rmi X.dkr.ecr.ap-southeast-2.amazonaws.com/mathgame'
 alias mathrun='ecrlogin && docker run -d --name=mathgame --publish=80:5000 --env-file ./.env --restart unless-stopped X.dkr.ecr.ap-southeast-2.amazonaws.com/mathgame:latest'
