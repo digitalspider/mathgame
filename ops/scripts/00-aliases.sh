0dfe7e2886759fbef54bbe8c8035fcfdd128679f
David Vittor <david.vittor@cadreon.com>
2019-09-15 22:51:22 +1000

    Updated scripts

--- ops/scripts/00-aliases.sh
+++ ops/scripts/00-aliases.sh
@@ -4,7 +4,7 @@
 alias upgrade='sudo apt update && sudo apt -y upgrade'
 
 # MATHGAME Docker commands
-alias math='docker exec -it mathgame /bin/sh -l'
+alias mathgame='docker exec -it mathgame /bin/sh -l'
 alias mathstatus='docker ps --filter "name=mathgame"'
 alias mathstart='docker start mathgame'
 alias mathstop='docker stop mathgame'
@@ -15,8 +15,8 @@ alias mathlog='docker logs -t -f mathgame'
 alias ecrlogin='$(aws --profile mathgame ecr get-login --no-include-email --region ap-southeast-2)'
 alias mathclean='docker rm -f mathgame && docker rmi X.dkr.ecr.ap-southeast-2.amazonaws.com/mathgame'
 alias mathrun='ecrlogin && docker run -d --name=mathgame --publish=80:5000 --env-file ./.env --restart unless-stopped X.dkr.ecr.ap-southeast-2.amazonaws.com/mathgame:latest'
-alias s3pull='aws --profile mathgame s3 cp s3://agt-prod/.env .'
-alias s3push='aws --profile mathgame s3 cp .env s3://agt-prod/.env --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers'
+alias s3pull='aws --profile mathgame s3 cp s3://mathgame/.env .'
+alias s3push='aws --profile mathgame s3 cp .env s3://mathgame/.env --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers'
 
 # PSQL commands
 alias dblogin='export PGPASSWORD="password"'
