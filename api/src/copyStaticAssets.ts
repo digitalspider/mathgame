ad97c7cdd562eade4ed6294ad7f379bf479a2007
Development Team <dev@digitalspider.com.au>
2019-09-22 23:57:30 +1000

    Removed before copy

--- api/src/copyStaticAssets.ts
+++ api/src/copyStaticAssets.ts
@@ -1,4 +1,6 @@
 import * as shell from "shelljs";
 
+shell.rm('-rf', 'dist/public/');
+shell.rm('-rf', 'dist/views/');
 shell.cp("-R", "src/public/", "dist/public/");
 shell.cp("-R", "src/views/", "dist/views/");
