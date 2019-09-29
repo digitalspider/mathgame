62f2302c605e579b51493090c985f1c5e5b0aa18
Development Team <dev@digitalspider.com.au>
2019-09-29 18:22:00 +1000

    Slightly updated ops scripts

--- ops/scripts/docker-build.sh
+++ ops/scripts/docker-build.sh
@@ -1,1 +1,7 @@
+#Local
 docker build . -t mathgame
+
+$(aws --profile mathgame ecr get-login --no-include-email --region ap-southeast-2)
+export buildid=$(aws --profile mathgame ecr list-images --repository mathgame | grep imageTag | grep build | cut -f 4 -d '"' | sort -rn | head -1)
+docker tag mathgame:latest 640016856401.dkr.ecr.ap-southeast-2.amazonaws.com/mathgame:build-9
+docker push 640016856401.dkr.ecr.ap-southeast-2.amazonaws.com/mathgame:build-9
