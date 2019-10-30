3094b7a59306e1e3368b94f4f2de2556347c0817
Development Team <dev@digitalspider.com.au>
2019-10-30 18:02:07 +1100

    Docker env

--- ops/scripts/docker-run.sh
+++ ops/scripts/docker-run.sh
@@ -1,2 +1,2 @@
 # Local
-docker run --name mathgame --publish 5000:5000 --env-file=api/.env --add-host dockerhost:172.17.0.1 mathgame
+docker run --name mathgame --publish 5000:5000 --env-file=api/.env.docker --add-host dockerhost:172.17.0.1 mathgame
