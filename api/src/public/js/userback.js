cdd6d41772970e40096fe2ac304c72ce03bd9421
Development Team <dev@digitalspider.com.au>
2019-11-03 00:26:06 +1100

    Userback feedback

---
+++ api/src/public/js/userback.js
@@ -0,0 +1,7 @@
+Userback = window.Userback || {};
+Userback.access_token = '8053|13466|b8SeqGiDkP84gIT5dcZxSf4p3TbhW5kmlWMsNlch35zzVUo6Yh';
+(function(id) {
+    var s = document.createElement('script');
+    s.async = 1;s.src = 'https://static.userback.io/widget/v1.js';
+    var parent_node = document.head || document.body;parent_node.appendChild(s);
+})('userback-sdk');
