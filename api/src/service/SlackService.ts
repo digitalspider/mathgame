6e8066aa57f7ddab7e6520935eba78bf78a4719d
Development Team <dev@digitalspider.com.au>
2019-11-03 03:11:38 +1100

    Slack notifications working

--- api/src/service/SlackService.ts
+++ api/src/service/SlackService.ts
@@ -1,23 +1,21 @@
 import { Service } from 'typedi';
 import { SLACK_POST_URL } from '../config';
+import Axios, { AxiosResponse } from 'axios';
 
 @Service()
 class SlackService {
 
   async sendMessage(message: string): Promise<void> {
-    console.log(`sendMessage called. ${SLACK_POST_URL}. ${message}`);
     if (SLACK_POST_URL) {
-      const fetchData: RequestInit = {
-        method: 'POST',
-        body: JSON.stringify({
-          text: message,
-        }),
-      };
+      const postData = JSON.stringify({
+        text: message,
+      });
       try {
         // send fetch data
         console.log(`sending message to slack: ${message}`);
-        let response: Response = await fetch(SLACK_POST_URL, fetchData);
-        if (!response.ok) {
+        // Performing a POST request
+        let response: AxiosResponse = await Axios.post(SLACK_POST_URL, postData);
+        if (response.status !== 200) {
           console.error(`Could not update Slack. status=${response.status}. text=${response.statusText}`);
         }
       } catch (err) {
